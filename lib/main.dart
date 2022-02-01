79e22dfc2d37f570ebd036c23715c780975f31fb
Nhesneil <nhesneil@gmail.com>
2022-01-31 21:32:10 -0500

    More smooth

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
-import 'package:async/async.dart';
+//import 'package:async/async.dart';
 
 void main() {
+
   runApp(MyApp());
 }
 
@@ -12,58 +13,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sensors Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
+  //MyHomePage({Key? key, this.title}) : super(key: key);
 
-  final String? title;
+  //final String? title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+Color backGround = Colors.red;
+
 class _MyHomePageState extends State<MyHomePage> {
   List<double>?
       _accelerometerValues; // question mark means that value can be null
-  List<double>?
-      _userAccelerometerValues; //.... means you dont need to initialize it
+  List<double> _userAccelerometerValues=[0]; //.... means you dont need to initialize it
   final _streamSubscriptions = <StreamSubscription<dynamic>>[];
 
-  Color backGround = Colors.green;
-
+  double threshold=2;
+  Color feedback() {
+    setState(() {
+      if (_userAccelerometerValues[0] < threshold){
+        if (_userAccelerometerValues[0]  >1) {
+          backGround = Colors.red;
+        }
+      }
+      if (_userAccelerometerValues[0] > threshold){
+        backGround = Colors.green;
+      }
+    });
+    return backGround;
+  }
   @override
   Widget build(BuildContext context) {
     final accelerometer =
         _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
     final userAccelerometer = _userAccelerometerValues
-        ?.map((double v) => v.toStringAsFixed(1))
+        .map((double v) => v.toStringAsFixed(1))
         .toList();
 
     return Scaffold(
-      backgroundColor: backGround,
-      appBar: AppBar(
-        title: const Text('Sensor Example'),
-      ),
+      backgroundColor: feedback(),
+      // appBar: AppBar(
+      //
+      // ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          // Padding(
-          //   padding: const EdgeInsets.all(16.0),
-          //   child: Row(
-          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //     children: <Widget>[
-          //       Text('Accelerometer: $accelerometer'),
-          //     ],
-          //   ),
-          // ),
           Expanded(
             child: Padding(
               padding:
@@ -84,41 +85,22 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
   @override
   void dispose() {
+
     super.dispose();
     for (final subscription in _streamSubscriptions) {
       subscription.cancel();
     }
   }
-
   @override
   void initState() {
     super.initState();
-    // _streamSubscriptions.add(
-    //   accelerometerEvents.listen(
-    //         (AccelerometerEvent event) {
-    //       setState(() {
-    //
-    //         _accelerometerValues = <double>[event.x, event.y, event.z];
-    //
-    //
-    //       });
-    //     },
-    //   ),
-    // );
     _streamSubscriptions.add(
       userAccelerometerEvents.listen(
         (UserAccelerometerEvent event) {
           setState(() {
             _userAccelerometerValues = <double>[event.y];
-
-            if (event.y < 3) {
-              backGround = Colors.red;
-            } else {
-              backGround = Colors.green;
-            }
           });
         },
       ),

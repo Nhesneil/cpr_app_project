da04d0f8b6b201c7607b682e1a16f68123740ef3
Nhesneil <nhesneil@gmail.com>
2022-03-21 16:19:34 -0400

    For testing or analysis purposes

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(
-                      'Acceleration Thresold',
+                      'Acceleration Threshold',
                       style: kLabelTextStyle,
                     ),
                     Row(
